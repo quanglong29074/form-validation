c9986064b8c063a78886d37e8a5dc9c1a9cae17d
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2022-11-23 15:20:14 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const passwordEl = document.querySelector('#password');
+const confirmPasswordEl = document.querySelector('#confirm-password');
+
+const form = document.querySelector('#signup');
+
+
+const checkUsername = () => {
+
+    let valid = false;
+
+    const min = 3;
+       max = 25;
+
+      const username = usernameEl.value.trim();
+      
+      if (!isRequired(username)) {
+          showError(usernameEl, 'Username cannot be blank.');
+      } else if (!isBetween(username.length, min, max)) {
+        showError(usernameEl, `Username must be between ${min} and ${max} characters.`)
+      } else {
+        showSuccess(usernameEl);
+        valid = true;
+      }
+      return valid;
+};
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl,'Email is not vaid.')
+    } else {
+      showSuccess(emailEl);
+      valid = true;
+    }
+    return valid;
+};
+
+const checkPassword = ()=> {
+  let valid = false;
+
+
+  const password = passwordEl.value.trim();
+
+  if (!isRequired(password)) {
+    showError(passwordEl, 'password cannot be blank.');
+  }else if (!isPasswordSecure(password)) {
+    showError(passwordEl, 'password must has at least 8 characters that inclue at least 1 lowercase'+
+    'character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
+  } else {
+    showSuccess(passwordEl);
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkConfirmPassword = ()=> {
+  let valid = false;
+  //check confirm password
+  const confirmPassword = confirmPasswordEl.value.trim();
+  const password = passwordEl.value.trim();
+
+  if (!isRequired(confirmPassword)) {
+    showError(confirmPasswordEl, 'please enter the password again');
+  }else if (password !== confirmPassword) {
+    showError(confirmPasswordEl, 'The password dose not match');
+  } else {
+    showSuccess(confirmPasswordEl);
+    valid = true;
+  }
+
+  return valid;
+};
+
+const isEmailValid = (email) => {
+  //regular expression (check email)
+  //https://developer.mozzila.org/en-US/docs/Web/Javascript/Reference/Global_Objects/Regexp
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const isPasswordSecure = (password) => {
+  //regular expression (check password)
+  const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+  return re.test(password);
+};
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+
+const showError = (input, message) => {
+  //get the from-lield element
+  const formField = input.parentElement;
+  //add the error class
+  formField.classList.remove('success');
+  formField.classList.add('error');
+
+  //show the error message
+  const error = formField.querySelector('small');
+  error.textContent = message;
+};
+
+const showSuccess = (input) => {
+  //get the fro,-field element
+  const formField = input.parentElement;
+
+  //remove the error class
+  formField.classList.remove('success');
+  formField.classList.add('error');
+
+  //hide the error message
+  const error = formField.querySelector('small');
+  error.textContent = '';
+}
+
+
+form.addEventListener('submit', function (e) {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // validate fields
+  let isUsernameValid = checkUsername(),
+       isEmailValid = checkEmail(),
+       isPasswordValid = checkPassword(),
+       isConfirmPasswordValid = checkConfirmPassword();
+  let isFormValid = isUsernameValid &&
+      isEmailValid &&
+      isPasswordValid &&
+      isConfirmPasswordValid;
+
+      // submit to the server if the form is valid
+      if (isFormValid) {
+
+      }
+});
+const debounce = (fn, delay = 1) => {
+  let timeoutId;
+  return (...args) => {
+    // cancel the previous timer 
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    // setup a new timer
+    timeoutId = setTimeout(() => {
+      fn.apply(null, args)
+
+    }, delay);
+  };
+};
+form.addEventListener('input', debounce(function (e) {
+  switch (e.target.id) {
+    case 'username':
+        checkUsername();
+        break;
+    case 'email':
+      checkEmail();
+      break;
+    case 'password':
+      checkPassword();
+      break;
+    case 'confirm-password':
+      checkConfirmPassword();
+      break;
+  }
+}));
